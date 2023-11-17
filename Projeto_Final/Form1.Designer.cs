fbd4a1967c422566096083e24f680cc03288fd75
Walter Dias Marques Pereira <walter.dmp@hotmail.com>
2023-11-17 13:59:36 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Final/Form1.Designer.cs
@@ -0,0 +1,472 @@
+﻿namespace Projeto_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            tabAlterar = new TabPage();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox3 = new TextBox();
+            comboBox2 = new ComboBox();
+            button5 = new Button();
+            tabBuscar = new TabPage();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            tabCadastrar = new TabPage();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            comboBox1 = new ComboBox();
+            button3 = new Button();
+            button4 = new Button();
+            tabControl1 = new TabControl();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabAlterar.SuspendLayout();
+            tabBuscar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            tabCadastrar.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(793, 148);
+            panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ButtonHighlight;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Font = new Font("Segoe UI Semibold", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(157, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(567, 67);
+            label2.TabIndex = 1;
+            label2.Text = "Mercado Nova Machado";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.GrayText;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(78, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(82, 67);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // tabAlterar
+            // 
+            tabAlterar.BackColor = Color.LightGray;
+            tabAlterar.Controls.Add(button5);
+            tabAlterar.Controls.Add(comboBox2);
+            tabAlterar.Controls.Add(textBox3);
+            tabAlterar.Controls.Add(textBox6);
+            tabAlterar.Controls.Add(textBox7);
+            tabAlterar.Controls.Add(label7);
+            tabAlterar.Controls.Add(label8);
+            tabAlterar.Controls.Add(label9);
+            tabAlterar.Controls.Add(label10);
+            tabAlterar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tabAlterar.Location = new Point(4, 34);
+            tabAlterar.Name = "tabAlterar";
+            tabAlterar.Padding = new Padding(3);
+            tabAlterar.Size = new Size(792, 264);
+            tabAlterar.TabIndex = 2;
+            tabAlterar.Text = "Alterar";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(11, 38);
+            label10.Name = "label10";
+            label10.Size = new Size(78, 21);
+            label10.TabIndex = 10;
+            label10.Text = "Produto: ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(11, 80);
+            label9.Name = "label9";
+            label9.Size = new Size(85, 21);
+            label9.TabIndex = 11;
+            label9.Text = "Categoria:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(11, 127);
+            label8.Name = "label8";
+            label8.Size = new Size(99, 21);
+            label8.TabIndex = 12;
+            label8.Text = "Quantidade:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(11, 170);
+            label7.Name = "label7";
+            label7.Size = new Size(56, 21);
+            label7.TabIndex = 13;
+            label7.Text = "Preço:";
+            // 
+            // textBox7
+            // 
+            textBox7.BorderStyle = BorderStyle.FixedSingle;
+            textBox7.Location = new Point(127, 35);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(558, 29);
+            textBox7.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Location = new Point(127, 124);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(558, 29);
+            textBox6.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(127, 168);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(558, 29);
+            textBox3.TabIndex = 16;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(127, 77);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(558, 29);
+            comboBox2.TabIndex = 17;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(296, 203);
+            button5.Name = "button5";
+            button5.Size = new Size(205, 44);
+            button5.TabIndex = 18;
+            button5.Text = "Confirmar Alteração";
+            button5.TextAlign = ContentAlignment.MiddleRight;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // tabBuscar
+            // 
+            tabBuscar.BackColor = Color.LightGray;
+            tabBuscar.Controls.Add(dataGridView1);
+            tabBuscar.Controls.Add(button2);
+            tabBuscar.Controls.Add(button1);
+            tabBuscar.Controls.Add(textBox1);
+            tabBuscar.Controls.Add(label1);
+            tabBuscar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tabBuscar.Location = new Point(4, 34);
+            tabBuscar.Name = "tabBuscar";
+            tabBuscar.Padding = new Padding(3);
+            tabBuscar.Size = new Size(792, 264);
+            tabBuscar.TabIndex = 1;
+            tabBuscar.Text = "Buscar";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(39, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Produto:";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(114, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(283, 29);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(477, 15);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 45);
+            button1.TabIndex = 2;
+            button1.Text = "Remover";
+            button1.TextAlign = ContentAlignment.MiddleRight;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(621, 15);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 45);
+            button2.TabIndex = 3;
+            button2.Text = "Alterar";
+            button2.TextAlign = ContentAlignment.MiddleRight;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(39, 66);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(715, 195);
+            dataGridView1.TabIndex = 4;
+            // 
+            // tabCadastrar
+            // 
+            tabCadastrar.BackColor = Color.LightGray;
+            tabCadastrar.Controls.Add(button4);
+            tabCadastrar.Controls.Add(button3);
+            tabCadastrar.Controls.Add(comboBox1);
+            tabCadastrar.Controls.Add(textBox5);
+            tabCadastrar.Controls.Add(textBox4);
+            tabCadastrar.Controls.Add(textBox2);
+            tabCadastrar.Controls.Add(label6);
+            tabCadastrar.Controls.Add(label5);
+            tabCadastrar.Controls.Add(label4);
+            tabCadastrar.Controls.Add(label3);
+            tabCadastrar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tabCadastrar.ForeColor = SystemColors.ControlText;
+            tabCadastrar.Location = new Point(4, 34);
+            tabCadastrar.Name = "tabCadastrar";
+            tabCadastrar.Padding = new Padding(3);
+            tabCadastrar.RightToLeft = RightToLeft.No;
+            tabCadastrar.Size = new Size(792, 264);
+            tabCadastrar.TabIndex = 0;
+            tabCadastrar.Text = "Cadastrar";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(11, 39);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 21);
+            label3.TabIndex = 0;
+            label3.Text = "Produto: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(11, 81);
+            label4.Name = "label4";
+            label4.Size = new Size(85, 21);
+            label4.TabIndex = 1;
+            label4.Text = "Categoria:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(11, 128);
+            label5.Name = "label5";
+            label5.Size = new Size(99, 21);
+            label5.TabIndex = 2;
+            label5.Text = "Quantidade:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(11, 171);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 21);
+            label6.TabIndex = 3;
+            label6.Text = "Preço:";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(127, 36);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(558, 29);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Location = new Point(127, 125);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(558, 29);
+            textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Location = new Point(127, 169);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(558, 29);
+            textBox5.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(127, 78);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(558, 29);
+            comboBox1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(327, 204);
+            button3.Name = "button3";
+            button3.Size = new Size(127, 44);
+            button3.TabIndex = 9;
+            button3.Text = "Cadastrar";
+            button3.TextAlign = ContentAlignment.MiddleRight;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.LightGray;
+            button4.BackgroundImageLayout = ImageLayout.None;
+            button4.FlatAppearance.BorderColor = Color.LightGray;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(695, 73);
+            button4.Name = "button4";
+            button4.Size = new Size(47, 37);
+            button4.TabIndex = 10;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabCadastrar);
+            tabControl1.Controls.Add(tabBuscar);
+            tabControl1.Controls.Add(tabAlterar);
+            tabControl1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            tabControl1.ItemSize = new Size(100, 30);
+            tabControl1.Location = new Point(-3, 147);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 302);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+            tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabAlterar.ResumeLayout(false);
+            tabAlterar.PerformLayout();
+            tabBuscar.ResumeLayout(false);
+            tabBuscar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tabCadastrar.ResumeLayout(false);
+            tabCadastrar.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private TabPage tabAlterar;
+        private Button button5;
+        private ComboBox comboBox2;
+        private TextBox textBox3;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TabPage tabBuscar;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private TabPage tabCadastrar;
+        private Button button4;
+        private Button button3;
+        private ComboBox comboBox1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private TabControl tabControl1;
+    }
+}
